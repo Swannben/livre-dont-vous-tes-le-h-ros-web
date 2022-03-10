75271abf89f2cf15cf33a7b4604bda4ae255409c
Swann <swannbenz@gmail.com>
2022-03-10 19:53:56 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+﻿
+
+
+
+window.addEventListener("load", function(){
+	var numchap=1;
+	if (window.location.hash.substring(1)){
+		numchap=window.location.hash.substring(1);
+	}
+	
+	update(numchap);
+	
+	});
+
+window.addEventListener("hashchange", function(){
+	var numchap=window.location.hash.substring(1);
+
+	update(numchap);
+})
+
+
+function update( numchap){
+	fetch("json/chapitre"+numchap+".json")
+	.then(function (response){
+		return response.json();
+	}).then(function(data){
+		var paragraphe =document.getElementById("paragraphe");
+		var list =document.getElementById("listeLien");
+		paragraphe.textContent=data.txt;
+		while (list.firstChild){
+			list.removeChild(list.firstChild);
+		}
+		for (element of data.links){
+			ea= document.createElement("a");
+			ea.setAttribute("href",element.link);
+			ea.textContent=element.txt;
+			eli= document.createElement("li");
+			eli.appendChild(ea);
+			list.appendChild(eli);
+		}
+	})
+}	
